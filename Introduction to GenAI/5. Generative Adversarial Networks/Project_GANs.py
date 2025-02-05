4ba1be8d61b75dee08aa7e75fc01376bd931c699
Aryan Parekh <aryanparekh2412@gmail.com>
2025-02-05 10:10:26 -0500

    GANs code added

---
+++ Introduction to GenAI/5. Generative Adversarial Networks/Project_GANs.py
@@ -0,0 +1,142 @@
+# This code works, but it needs to run for 2000 epochs to build the model.
+
+
+# Import necessary libraries
+import tensorflow as tf
+import tensorflow_datasets as tfds
+from matplotlib import pyplot as plt
+import numpy as np
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, Dense, Flatten, Reshape, LeakyReLU, Dropout, UpSampling2D
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.losses import BinaryCrossentropy
+from tensorflow.keras.callbacks import Callback
+import os
+from tensorflow.keras.preprocessing.image import array_to_img
+
+# Load and preprocess the dataset
+def load_and_preprocess_data():
+    ds = tfds.load('fashion_mnist', split='train')
+    ds = ds.map(lambda data: data['image'] / 255)
+    ds = ds.cache()
+    ds = ds.shuffle(60000)
+    ds = ds.batch(128)
+    ds = ds.prefetch(64)
+    return ds
+
+# Build the generator model
+def build_generator():
+    model = Sequential()
+    model.add(Dense(7*7*128, input_dim=128))
+    model.add(LeakyReLU(0.2))
+    model.add(Reshape((7,7,128)))
+    model.add(UpSampling2D())
+    model.add(Conv2D(128, 5, padding='same'))
+    model.add(LeakyReLU(0.2))
+    model.add(UpSampling2D())
+    model.add(Conv2D(128, 5, padding='same'))
+    model.add(LeakyReLU(0.2))
+    model.add(Conv2D(128, 4, padding='same'))
+    model.add(LeakyReLU(0.2))
+    model.add(Conv2D(128, 4, padding='same'))
+    model.add(LeakyReLU(0.2))
+    model.add(Conv2D(1, 4, padding='same', activation='sigmoid'))
+    return model
+
+# Build the discriminator model
+def build_discriminator():
+    model = Sequential()
+    model.add(Conv2D(32, 5, input_shape=(28,28,1)))
+    model.add(LeakyReLU(0.2))
+    model.add(Dropout(0.4))
+    model.add(Conv2D(64, 5))
+    model.add(LeakyReLU(0.2))
+    model.add(Dropout(0.4))
+    model.add(Conv2D(128, 5))
+    model.add(LeakyReLU(0.2))
+    model.add(Dropout(0.4))
+    model.add(Conv2D(256, 5))
+    model.add(LeakyReLU(0.2))
+    model.add(Dropout(0.4))
+    model.add(Flatten())
+    model.add(Dropout(0.4))
+    model.add(Dense(1, activation='sigmoid'))
+    return model
+
+# Define the GAN model
+class FashionGAN(tf.keras.Model):
+    def __init__(self, generator, discriminator, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.generator = generator
+        self.discriminator = discriminator
+
+    def compile(self, g_opt, d_opt, g_loss, d_loss, *args, **kwargs):
+        super().compile(*args, **kwargs)
+        self.g_opt = g_opt
+        self.d_opt = d_opt
+        self.g_loss = g_loss
+        self.d_loss = d_loss
+
+    def train_step(self, batch):
+        real_images = batch
+        fake_images = self.generator(tf.random.normal((128, 128, 1)), training=False)
+
+        with tf.GradientTape() as d_tape:
+            yhat_real = self.discriminator(real_images, training=True)
+            yhat_fake = self.discriminator(fake_images, training=True)
+            yhat_realfake = tf.concat([yhat_real, yhat_fake], axis=0)
+            y_realfake = tf.concat([tf.zeros_like(yhat_real), tf.ones_like(yhat_fake)], axis=0)
+            noise_real = 0.15 * tf.random.uniform(tf.shape(yhat_real))
+            noise_fake = -0.15 * tf.random.uniform(tf.shape(yhat_fake))
+            y_realfake += tf.concat([noise_real, noise_fake], axis=0)
+            total_d_loss = self.d_loss(y_realfake, yhat_realfake)
+
+        dgrad = d_tape.gradient(total_d_loss, self.discriminator.trainable_variables)
+        self.d_opt.apply_gradients(zip(dgrad, self.discriminator.trainable_variables))
+
+        with tf.GradientTape() as g_tape:
+            gen_images = self.generator(tf.random.normal((128,128,1)), training=True)
+            predicted_labels = self.discriminator(gen_images, training=False)
+            total_g_loss = self.g_loss(tf.zeros_like(predicted_labels), predicted_labels)
+
+        ggrad = g_tape.gradient(total_g_loss, self.generator.trainable_variables)
+        self.g_opt.apply_gradients(zip(ggrad, self.generator.trainable_variables))
+
+        return {"d_loss": total_d_loss, "g_loss": total_g_loss}
+
+# Define the callback to monitor training
+class ModelMonitor(Callback):
+    def __init__(self, num_img=3, latent_dim=128):
+        self.num_img = num_img
+        self.latent_dim = latent_dim
+
+    def on_epoch_end(self, epoch, logs=None):
+        random_latent_vectors = tf.random.uniform((self.num_img, self.latent_dim, 1))
+        generated_images = self.model.generator(random_latent_vectors)
+        generated_images *= 255
+        generated_images.numpy()
+        for i in range(self.num_img):
+            img = array_to_img(generated_images[i])
+            img.save(os.path.join('images', f'generated_img_{epoch}_{i}.png'))
+
+# Main function to train the GAN
+def execute_gan():
+    ds = load_and_preprocess_data()
+    generator = build_generator()
+    discriminator = build_discriminator()
+    g_opt = Adam(learning_rate=0.0001)
+    d_opt = Adam(learning_rate=0.00001)
+    g_loss = BinaryCrossentropy()
+    d_loss = BinaryCrossentropy()
+    fashgan = FashionGAN(generator, discriminator)
+    fashgan.compile(g_opt, d_opt, g_loss, d_loss)
+    # hist = fashgan.fit(ds, epochs=2000, callbacks=[ModelMonitor()])
+    imgs = generator.predict(tf.random.normal((16, 128, 1)))
+    fig, ax = plt.subplots(ncols=4, nrows=4, figsize=(10,10))
+    for r in range(4): 
+        for c in range(4): 
+            ax[r][c].imshow(imgs[(r+1)*(c+1)-1])
+    plt.show()
+
+# Execute
+execute_gan()
